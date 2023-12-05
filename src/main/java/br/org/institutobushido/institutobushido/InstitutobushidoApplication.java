4cd9b995f3a06421f6dfd2ac40c79a01aaa3ddee
ana.aguirre <anafran27@gmail.com>
2023-12-04 22:26:16 -0400

    Configuração inicial do projeto

---
+++ src/main/java/br/org/institutobushido/institutobushido/InstitutobushidoApplication.java
@@ -0,0 +1,13 @@
+package br.org.institutobushido.institutobushido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutobushidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutobushidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/institutobushido/institutobushido/InstitutobushidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.institutobushido.institutobushido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutobushidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
