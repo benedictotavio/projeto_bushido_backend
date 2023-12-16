b83687b3516738cdfe2c3824996baf23249413c2
Otavio Benedicto <otavio.bene@outlook.com>
2023-12-16 04:24:05 -0300

    fix: indentação no App main

---
+++ src/main/java/br/org/institutobushido/InstitutobushidoApplication.java
@@ -0,0 +1,11 @@
+package br.org.institutobushido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutobushidoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutobushidoApplication.class, args);
+	}
+}
