c8d82f165df484bba030c95a493b41b64c9a569e
Otavio Benedicto <otavio.bene@outlook.com>
2023-12-21 03:03:04 -0300

    feat: novo payload com metodos de validação com validation spring

--- src/main/java/br/org/institutobushido/controllers/AlunoControllers.java
+++ src/main/java/br/org/institutobushido/controllers/AlunoControllers.java
@@ -5,9 +5,11 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.MongoException;
@@ -15,6 +17,7 @@ import com.mongodb.MongoException;
 import br.org.institutobushido.dtos.aluno.AlunoDTORequest;
 import br.org.institutobushido.dtos.aluno.AlunoDTOResponse;
 import br.org.institutobushido.services.aluno.AlunoServices;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/V1/aluno")
@@ -24,12 +27,21 @@ public class AlunoControllers {
     @Autowired
     private AlunoServices alunoServices;
 
+    @GetMapping()
+    ResponseEntity<Object> buscarAlunoPorEmail(@RequestParam(name = "rg") String rg) {
+    try {
+        AlunoDTOResponse alunoEncontrado = alunoServices.buscarAlunoPorRg(rg);
+        return ResponseEntity.ok().body(alunoEncontrado);
+    } catch (MongoException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
+
     @PostMapping()
-    ResponseEntity<?> adicionarAluno(@RequestBody AlunoDTORequest alunoDTORequest) {
+    ResponseEntity<String> adicionarAluno(@Valid @RequestBody AlunoDTORequest alunoDTORequest) {
         try {
             AlunoDTOResponse novoAluno = alunoServices.adicionarAluno(alunoDTORequest);
-            // Alterar URI
-            return ResponseEntity.created(URI.create("localhost")).body(novoAluno);
+            return ResponseEntity.created(URI.create("localhost")).body(novoAluno.rg());
         } catch (MongoException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/br/org/institutobushido/dtos/aluno/AlunoDTORequest.java
+++ src/main/java/br/org/institutobushido/dtos/aluno/AlunoDTORequest.java
@@ -2,27 +2,47 @@ package br.org.institutobushido.dtos.aluno;
 
 import java.util.Date;
 
+import com.mongodb.lang.NonNull;
+
 import br.org.institutobushido.enums.Imovel;
 import br.org.institutobushido.enums.TipoDeTransporte;
 import br.org.institutobushido.enums.Turno;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record AlunoDTORequest(
-        String nome,
+        @NotNull(message = "Nome é obrigatório") String nome,
+
         boolean bolsaFamilia,
+
         boolean auxilioBrasil,
+
         Imovel imovel,
+
         int numerosDePessoasNaCasa,
+
         int contribuintesDaRendaFamiliar,
+
         boolean alunoContribuiParaRenda,
+
         int rendaFamiliarEmSalariosMinimos,
+
         TipoDeTransporte transporte,
+
         boolean vemAcompanhado,
+
         Turno turno,
+
         Date dataPreenchimento,
+
         String cidade,
+
         String estado,
-        String rg,
+
+        @NonNull() @Pattern(regexp = "^\\d{9}$", message = "Formato de rg inválido!") String rg,
         String cpfResponsavel,
+
         int faltas,
+
         boolean status) {
 }
