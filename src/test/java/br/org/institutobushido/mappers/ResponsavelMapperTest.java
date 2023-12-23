378fa8ef9834a1117be9eba117246e64c86e7684
ana.aguirre <anafran27@gmail.com>
2023-12-23 13:01:49 -0400

    Removendo importações não utilizadas

--- src/test/java/br/org/institutobushido/mappers/ResponsavelMapperTest.java
+++ src/test/java/br/org/institutobushido/mappers/ResponsavelMapperTest.java
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
