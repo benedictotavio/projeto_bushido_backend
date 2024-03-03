805445aa0689fdbbb9193bfc3aae4ae8ffe21df4
Otavio Benedicto <otavio.bene@outlook.com>
2024-03-02 23:34:48 -0300

    security: liberar acesso para todos os servidores

--- src/main/java/br/org/institutobushido/controllers/aluno/AlunoControllers.java
+++ src/main/java/br/org/institutobushido/controllers/aluno/AlunoControllers.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,8 +32,9 @@ import br.org.institutobushido.resources.response.success.SuccessPostResponse;
 import br.org.institutobushido.services.aluno.AlunoServices;
 import jakarta.validation.Valid;
 
-@RestController
+@RestController(value = "aluno")
 @RequestMapping("api/V1/aluno")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AlunoControllers {
 
         @Autowired
